236ce59e4a749a89c2e95eec169c2f5188cc367f
edward <fanofparadox@gmail.com>
2022-04-07 23:33:10 +0300

    login styled

--- app/views/login.php
+++ app/views/login.php
@@ -1,9 +1,34 @@
-<form id="login" method="post" action="/admin/login">
-  <p class="login-text">Login:</p>
-  <input class="input" type="text" name="login">
-  <p class="login-text">Password:</p>
-  <input class="input" type="password" name="password">
-  <p id="submit-wrap">
-    <input type="submit" value="Submit">
-  </p>
+<form 
+  id="login" 
+  class="p-3 border border-3 rounded-3"
+  method="post" 
+  action="/admin/login">
+  <label class="form-label" for="login-input">
+    Login:
+  </label>
+  <input 
+    id="login-input"
+    class="form-control" 
+    type="text" 
+    name="login"
+    aria-describedby="login-help-block"
+    required>
+  <div class="invalid-feedback">Wrong login!</div>
+  <div id="login-help-block" class="form-text">Login: admin</div>
+  <label class="form-label" for="password-input">
+    Password:
+  </label>
+  <input 
+    id="password-input"
+    class="form-control" 
+    type="password" 
+    name="password"
+    aria-describedby="password-help-block"
+    required>
+  <div class="invalid-feedback">Wrong password!</div>
+  <div id="password-help-block" class="form-text">Password: admin</div>
+  <input
+    class="btn btn-primary" 
+    type="submit" 
+    value="Submit">
 </form>
